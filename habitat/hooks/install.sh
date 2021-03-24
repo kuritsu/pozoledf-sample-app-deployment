3759ce6468d7ad6747f9dd9fb076e25220c9e53f
Jeffrey Alvarez <Jeffrey_Alvarez@intuit.com>
2021-03-23 19:33:10 -0600

    Include logs and private registry credentials extension.

--- habitat/hooks/install.sh
+++ habitat/hooks/install.sh
@@ -2,4 +2,7 @@
 
 export KUBECONFIG=/home/hab/kubeconfig
 cd "{{pkg.svc_config_install_path}}"
+if [ -f "/home/hab/docker-private.sh" ]; then
+  bash /home/hab/docker-private.sh
+fi
 kubectl apply -k . || true
--- habitat/hooks/run.sh
+++ habitat/hooks/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export KUBECONFIG=/home/hab/kubeconfig
 while [ true ]; do
+  kubectl logs -f -lapp=app --all-containers=true -n pozoledf
   sleep 5s
 done
