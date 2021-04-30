d78b650c35e39e39164144c1b247f41228427238
Jeffrey Alvarez <kuritsu@gmail.com>
2021-04-29 21:59:00 -0500

    Include docker config when docker-private script exists.

--- habitat/hooks/install.sh
+++ habitat/hooks/install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 export KUBECONFIG=/home/hab/kubeconfig
+
 cd "{{pkg.svc_config_install_path}}"
 if [ -f "/home/hab/docker-private.sh" ]; then
+  # This script needs to set the docker credentials in /home/hab/.docker/config.json
   bash /home/hab/docker-private.sh
 fi
+
 kubectl apply -k . || true
+
+if [ -f "/home/hab/docker-private.sh" ]; then
+  kubectl create secret generic regcred \
+      -n pozoledf \
+      --from-file=.dockerconfigjson=~/.docker/config.json \
+      --type=kubernetes.io/dockerconfigjson || true
+fi
